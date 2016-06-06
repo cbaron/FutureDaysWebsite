817b992e03017a5b5218d11939289f7de6733e51
cbaron <topherbaron@gmail.com>
2016-06-06 15:49:54 -0400

    fixing some things

--- client/js/views/templates/home.js
+++ client/js/views/templates/home.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-module.exports = ( p ) => `
-<div>HOME</div>
-`
-=======
-let html = `<div>Future Days</div>`
-
-module.exports = () => html
->>>>>>> origin/stylus
+module.exports = ( p ) => `<div>Future Days</div>`
