019b764de773b5fbbfa778ef54d7738813f89de2
cbaron <topherbaron@gmail.com>
2016-08-05 15:13:43 -0400

    updated view creation

---
+++ client/js/factory/View.js
@@ -0,0 +1,14 @@
+module.exports = Object.create( {
+
+    create( name, opts ) {
+        return Object.create(
+            this.Views[ name.charAt(0).toUpperCase() + name.slice(1) ],
+            Object.assign( { template: { value: this.Templates[ name ] }, user: { value: this.User }, factory: { value: this } }, opts )
+        ).constructor()
+    },
+
+}, {
+    Templates: { value: require('../.TemplateMap') },
+    User: { value: require('../models/User' ) },
+    Views: { value: require('../.ViewMap') }
+} )
