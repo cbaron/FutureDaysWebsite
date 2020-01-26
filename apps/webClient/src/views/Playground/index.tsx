cc64bf1f36b69fe3924924a9446ef11e33e41695
Chris Baron <topherbaron@gmail.com>
2020-01-25 20:51:11 -0500

    spinnin

--- apps/webClient/src/views/Playground/index.tsx
+++ apps/webClient/src/views/Playground/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, ReactElement } from "react";
 import { makeStyles } from "@material-ui/styles";
-import SceneManager from "./SceneManager";
+import * as THREE from "three";
+import { FBXLoader } from "./FBXLoader";
+
+require("./inflate.min.js");
 
 interface Props {}
 
@@ -8,57 +11,114 @@ const useStyles = makeStyles(() => ({
   root: {
     position: "fixed",
     width: "100vw",
-    height: "100vh",
-    border: "5px solid red"
+    height: "100vh"
   }
 }));
 
+let camera;
+let renderer;
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 const stickItUpYourYeah = (threeJsElement: ReactElement) => {
-  const canvas = createCanvas(document, threeJsElement);
-  const sceneManager = SceneManager(canvas);
+  let thing;
+  const loader = new FBXLoader();
 
-  let canvasHalfWidth;
-  let canvasHalfHeight;
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
+  );
+  camera.position.set(0, 30, 100);
 
-  bindEventListeners();
-  render();
+  const scene = new THREE.Scene();
+  //scene.background = new THREE.Color(0xa0a0a0);
+  //scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
-  function createCanvas(document, container) {
-    const canvas = document.createElement("canvas");
-    container.appendChild(canvas);
-    return canvas;
+  const light = new THREE.HemisphereLight(0xffffff, 0x444444);
+  light.position.set(0, 200, 0);
+  scene.add(light);
+
+  const lightTwo = new THREE.DirectionalLight(0xffffff);
+  lightTwo.position.set(0, 200, 100);
+  lightTwo.castShadow = true;
+  lightTwo.shadow.camera.top = 180;
+  lightTwo.shadow.camera.bottom = -100;
+  lightTwo.shadow.camera.left = -120;
+  lightTwo.shadow.camera.right = 120;
+  scene.add(lightTwo);
+
+  var mesh = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2000, 2000),
+    new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+  );
+  mesh.rotation.x = -Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+
+  var grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+  grid.material.opacity = 0.2;
+  grid.material.transparent = true;
+  scene.add(grid);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  window.addEventListener("resize", onWindowResize, false);
+
+  threeJsElement.appendChild(renderer.domElement);
+
+  loader.load(
+    "Models/Buildings/Building_Stadium.fbx",
+    function(object) {
+      //mixer = new THREE.AnimationMixer(object);
+
+      console.log(object);
+      //var action = mixer.clipAction(object.animations[0]);
+      //action.play();
+
+      object.traverse(function(child) {
+        if (child.isMesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+      thing = object;
+      object.position.y = 5;
+      object.position.x = 5;
+      object.position.z = 5;
+      object.scale.y = 0.15;
+      object.scale.x = 0.15;
+      object.scale.z = 0.15;
+      scene.add(object);
+    },
+    undefined,
+    function(e) {
+      console.error(e);
+    }
+  );
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    if (thing) {
+      thing.rotation.x += 0.01;
+      thing.rotation.y += 0.02;
+    }
+
+    renderer.render(scene, camera);
   }
 
-  function bindEventListeners() {
-    window.onresize = resizeCanvas;
-    window.onmousemove = mouseMove;
-    resizeCanvas();
-  }
-
-  function resizeCanvas() {
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-
-    canvasHalfWidth = Math.round(canvas.offsetWidth / 2);
-    canvasHalfHeight = Math.round(canvas.offsetHeight / 2);
-
-    sceneManager.onWindowResize();
-  }
-
-  function mouseMove({ screenX, screenY }) {
-    sceneManager.onMouseMove(
-      screenX - canvasHalfWidth,
-      screenY - canvasHalfHeight
-    );
-  }
-
-  function render() {
-    requestAnimationFrame(render);
-    sceneManager.update();
-  }
+  animate();
 };
 
 const Playground: React.FC<Props> = ({}) => {
