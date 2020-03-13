bcb110109d2d3e78f3b3b396e041c9a60c613548
JamesKemp296 <jameskemp2796@yahoo.com>
2020-03-13 10:20:01 -0400

    WIP

--- apps/webClient/src/components/View/index.tsx
+++ apps/webClient/src/components/View/index.tsx
@@ -7,32 +7,93 @@ import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Logo from "../Logo";
+import transitions from "@material-ui/core/styles/transitions";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const RED = "rgb(177,32,41)";
 const RED_GRADIENT =
   "linear-gradient(45deg, rgba(177,32,41,1) 0%, rgba(244,121,32,1) 100%)";
-const GREEN = "rgb(14,124,63)";
 const GREEN_GRADIENT =
   "linear-gradient(45deg, rgba(14,124,63,1) 0%, rgba(190,198,64,1) 100%)";
-
-const BLUE = "rgb(28,85,119)";
 const BLUE_GRADIENT =
   "linear-gradient(45deg, rgba(28,85,119,1) 0%, rgba(46,192,209,1) 100%)";
-const YELLOW = "rgb(190,121,42)";
 const YELLOW_GRADIENT =
   "linear-gradient(45deg, rgba(190,121,42,1) 0%, rgba(254,228,104,1) 100%)";
 
+// Home Colors
+const DARK_ORANGE = "#B12029";
+const LIGHT_ORANGE = "#F47920";
+
+// About colors
+const DARK_BLUE = "#1C5577";
+const LIGHT_BLUE = "#2EC0D1";
+
+// Our Work colors
+const DARK_GREEN = "#0E7C3F";
+const LIGHT_GREEN = "#BEC640";
+
+// About colors
+const DARK_YELLOW = "#BE792A";
+const LIGHT_YELLOW = "#FEE468";
+
 const backgroundTransitionsMap = {
   home: {
-    about: [
-      //  [intermediateBottomLeftColor, intermediateUupperRightColor],
-      //  [finalBL, finalupperRight ]
+    ourWork: [
+      [LIGHT_GREEN, DARK_ORANGE],
+      [DARK_GREEN, LIGHT_GREEN],
     ],
-    // ourWork: [ .. ]
+    about: [
+      [LIGHT_BLUE, DARK_ORANGE],
+      [DARK_BLUE, LIGHT_BLUE],
+    ],
+    letsTalk: [
+      [LIGHT_YELLOW, DARK_ORANGE],
+      [DARK_YELLOW, LIGHT_YELLOW],
+    ],
+  },
+  ourWork: {
+    home: [
+      [LIGHT_ORANGE, DARK_GREEN],
+      [DARK_ORANGE, LIGHT_ORANGE],
+    ],
+    about: [
+      [LIGHT_BLUE, DARK_GREEN],
+      [DARK_BLUE, LIGHT_BLUE],
+    ],
+    letsTalk: [
+      [LIGHT_YELLOW, DARK_GREEN],
+      [DARK_YELLOW, LIGHT_YELLOW],
+    ],
+  },
+  about: {
+    ourWork: [
+      [LIGHT_GREEN, DARK_BLUE],
+      [DARK_GREEN, LIGHT_GREEN],
+    ],
+    home: [
+      [LIGHT_ORANGE, DARK_BLUE],
+      [DARK_ORANGE, LIGHT_ORANGE],
+    ],
+    letsTalk: [
+      [LIGHT_YELLOW, DARK_BLUE],
+      [DARK_YELLOW, LIGHT_YELLOW],
+    ],
+  },
+  letsTalk: {
+    ourWork: [
+      [LIGHT_GREEN, DARK_YELLOW],
+      [DARK_GREEN, LIGHT_GREEN],
+    ],
+    about: [
+      [LIGHT_BLUE, DARK_YELLOW],
+      [DARK_BLUE, LIGHT_BLUE],
+    ],
+    home: [
+      [LIGHT_ORANGE, DARK_YELLOW],
+      [DARK_ORANGE, LIGHT_ORANGE],
+    ],
   },
 };
 
@@ -51,20 +112,20 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
   },
   redPageWrapper: {
-    backgroundColor: RED,
-    background: RED_GRADIENT,
+    backgroundColor: DARK_ORANGE,
+    backgroundImage: RED_GRADIENT,
   },
   greenPageWrapper: {
-    backgroundColor: GREEN,
-    background: GREEN_GRADIENT,
+    backgroundColor: DARK_GREEN,
+    backgroundImage: GREEN_GRADIENT,
   },
   bluePageWrapper: {
-    backgroundColor: BLUE,
-    background: BLUE_GRADIENT,
+    backgroundColor: DARK_BLUE,
+    backgroundImage: BLUE_GRADIENT,
   },
   yellowPageWrapper: {
-    backgroundColor: YELLOW,
-    background: YELLOW_GRADIENT,
+    backgroundColor: DARK_YELLOW,
+    backgroundImage: YELLOW_GRADIENT,
   },
 }));
 
